7fd289706059c5367b14aeab7d58241ab6495553
BeanFeed <two.austin.two@gmail.com>
2023-02-23 13:08:05 -0500

    Add assets and data

--- src/main/java/com/beanfeed/tdsword/Tdsword.java
+++ src/main/java/com/beanfeed/tdsword/Tdsword.java
@@ -3,6 +3,7 @@ package com.beanfeed.tdsword;
 import net.fabricmc.api.ModInitializer;
 
 public class Tdsword implements ModInitializer {
+    public static final String MODID = "tdsword";
     @Override
     public void onInitialize() {
 
