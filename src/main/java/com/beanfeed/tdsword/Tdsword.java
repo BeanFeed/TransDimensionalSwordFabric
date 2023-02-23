46667f99629c51d7800481c80e74c9671c12a294
BeanFeed <two.austin.two@gmail.com>
2023-02-23 13:01:28 -0500

    Initial commit

---
+++ src/main/java/com/beanfeed/tdsword/Tdsword.java
@@ -0,0 +1,10 @@
+package com.beanfeed.tdsword;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Tdsword implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
