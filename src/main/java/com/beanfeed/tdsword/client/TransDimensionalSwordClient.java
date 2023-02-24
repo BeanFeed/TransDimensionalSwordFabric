8e41d2340f6acb4b360453dbe718526536b5ead8
BeanFeed <two.austin.two@gmail.com>
2023-02-24 15:59:27 -0500

    Added model modifiers

--- src/main/java/com/beanfeed/tdsword/client/TransDimensionalSwordClient.java
+++ src/main/java/com/beanfeed/tdsword/client/TransDimensionalSwordClient.java
@@ -1,6 +1,8 @@
 package com.beanfeed.tdsword.client;
 
+import com.beanfeed.tdsword.TransDimensionalSword;
 import com.beanfeed.tdsword.entity.TDEntityTypes;
+import com.beanfeed.tdsword.item.TDItems;
 import com.beanfeed.tdsword.screen.TDScreenHandlers;
 import com.beanfeed.tdsword.screen.TDSscreen;
 import net.fabricmc.api.ClientModInitializer;
@@ -8,6 +10,13 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.minecraft.client.gui.screen.ingame.HandledScreens;
+import net.minecraft.client.item.ModelPredicateProviderRegistry;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.Nullable;
 import qouteall.imm_ptl.core.render.PortalEntityRenderer;
 
 @Environment(EnvType.CLIENT)
@@ -17,6 +26,18 @@ public class TransDimensionalSwordClient implements ClientModInitializer {
         EntityRendererRegistry.register(TDEntityTypes.TEMP_PORTAL, PortalEntityRenderer::new);
 
         HandledScreens.register(TDScreenHandlers.TD_SWORD_MENU, TDSscreen::new);
+
+        ModelPredicateProviderRegistry.register(TDItems.Rune, new Identifier("tdsword:nblank"), (itemStack, clientWorld, livingEntity, seed) -> {
+            NbtCompound nbt = itemStack.getOrCreateNbt();
+            if(nbt.contains("waypoint")) return 1;
+            return 0;
+        });
+
+        ModelPredicateProviderRegistry.register(TDItems.TDSword, new Identifier("tdsword:tdsunlit"), (itemStack, clientWorld, livingEntity, seed) -> {
+            NbtCompound nbt = itemStack.getOrCreateNbt();
+            if(nbt.contains("active") && nbt.getBoolean("active")) return 0;
+            return 1;
+        });
     }
 
     /*
@@ -31,5 +52,6 @@ public class TransDimensionalSwordClient implements ClientModInitializer {
     }
 
      */
+
 
 }
